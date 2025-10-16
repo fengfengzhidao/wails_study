b893b6dd7f30e9f524ada4638768c03159015c76
fengfeng <fengfengzhidao@126.com>
2025-10-16 20:22:31 +0800

    wails菜单-文件操作

--- wails_study/menu.go
+++ wails_study/menu.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/menu/keys"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"os"
 )
 
 func (a *App) getMenu() *menu.Menu {
@@ -25,6 +27,42 @@ func (a *App) getMenu() *menu.Menu {
 	moreMenu := m.AddSubmenu("更多")
 	moreMenu.AddText("关于", &keys.Accelerator{}, func(data *menu.CallbackData) {
 		fmt.Println("关于")
+	})
+	return m
+}
+
+func (a *App) getFileMenu() *menu.Menu {
+	m := menu.NewMenu()
+	fileMenu := m.AddSubmenu("文件")
+
+	fileMenu.AddText("打开文件", &keys.Accelerator{}, func(data *menu.CallbackData) {
+		fmt.Println("打开文件")
+		filePath, err := runtime.OpenFileDialog(a.ctx, runtime.OpenDialogOptions{
+			Title: "枫枫要选择文件了",
+			Filters: []runtime.FileFilter{
+				{
+					DisplayName: "Image Files (*.jpg, *.png)",
+					Pattern:     "*.jpg;*.png",
+				},
+			},
+		})
+		fmt.Println(filePath, err)
+	})
+	fileMenu.AddText("保存文件", &keys.Accelerator{}, func(data *menu.CallbackData) {
+		fmt.Println("保存文件")
+		filePath, err := runtime.SaveFileDialog(a.ctx, runtime.SaveDialogOptions{
+			Title:            "枫枫要保存文件了",
+			DefaultDirectory: "E:\\IT\\go_pro\\webview_study\\wails_study",
+			Filters: []runtime.FileFilter{
+				{
+					DisplayName: "Text Files (*.txt)",
+					Pattern:     "*.txt",
+				},
+			},
+		})
+		fmt.Println(filePath, err)
+		err = os.WriteFile(filePath, []byte("hello"), 0644)
+		fmt.Println(err)
 	})
 	return m
 }
